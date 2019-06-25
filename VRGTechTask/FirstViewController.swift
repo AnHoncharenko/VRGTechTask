d0bca112b03e90129432d044508e7fb9d354c2c2
AnHoncharenko <1goncharenkoanton@gmail.com>
2019-06-25 18:24:26 +0300

    Initial Commit

---
+++ VRGTechTask/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  VRGTechTask
+//
+//  Created by Anton Honcharenko on 6/25/19.
+//  Copyright © 2019 Anton Honcharenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VRGTechTask/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  VRGTechTask
+//
+//  Created by Anton Honcharenko on 6/25/19.
+//  Copyright © 2019 Anton Honcharenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
